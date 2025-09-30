fbf2b0bc13c3c256e63cc3825e597f02ead02577
zeox09 <jorgevillan06@gmail.com>
2025-09-30 07:18:04 -0300

    configuracion themes y css class in index.css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+export const Home = () => {
+    return <div className="min-h-screen">
+       {/* Theme toggle */}
+
+       {/*Background Effects*/ }
+
+       {/*NavBar*/ }
+
+       {/*Main Content*/ }
+
+       {/*Footer*/ }
+    </div>
+}
