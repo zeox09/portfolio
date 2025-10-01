d4da92ee985cb2709f064bdc9ca144a5462d72a4
zeox09 <jorgevillan06@gmail.com>
2025-10-01 01:20:42 -0300

    V 0.0.0.2

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
+import {cn} from "@/lib/utils"
 
 export const ThemeToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
@@ -27,7 +28,9 @@ export const ThemeToggle = () => {
     };
 
     return (
-        <button onClick={toggleTheme}>
+        <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 x-50 p-2 rounded-full transition-colors duration-300",
+            "focus:outline-hidden"
+        )}>
             {isDarkMode ? (
                 <Sun className="h-6 w-6 text-yellow-300" />
             ) : (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import { ThemeToggle } from "../components/themeToggle"
+import { ThemeToggle } from "../components/themeToggle";
+import {StarBackground} from "@/components/StarBackground";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
        {/* Theme toggle */}
         <ThemeToggle/>
        {/*Background Effects*/ }
-
+        <StarBackground/>
        {/*NavBar*/ }
 
        {/*Main Content*/ }
